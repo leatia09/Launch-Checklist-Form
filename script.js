e0894ef35cefddd37aa3361c08dd86bcf38f69cf
Leatia Stevenson <leatia09@icloud.com>
2019-10-14 19:09:39 -0500

    adding script

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 // Write your JavaScript code here!
-
+        window.addEventListener("load", function() {
+          let form = document.querySelector("form");
+          form.addEventListener("submit", function(event) {
+            let pilotNameInput = document.querySelector("input[name=pilotName]");
+             let coPilot = document.querySelector("input[name=coPilot]");
+             let fuel = document.querySelector("input[gas=fuel]");
+             let cargo = document.querySelector("input[weight=cargo]");
+             if (pilotNameInput.value === "" || coPilot.value === "" || fuel.value ==="" || cargo.value ==="") {
+                alert("All fields are required!");
+             }
+          });
+       });
+  
+}
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
